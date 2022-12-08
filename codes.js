6d7ddb6114b44d199fb12b8ac62c7c328e4e5dd9
Jodie Sauer <jodie.sauer@docc.techstarter.de>
2022-12-08 12:01:28 +0100

    devHell

--- codes.js
+++ codes.js
@@ -1,31 +1,61 @@
-let uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 class Password{
     
-    constructor(){
-        this.lowerCase = "abcdefghijklmnopqrstuvwxyz"
-        this.upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    }
-    // numbers   = "0123456789";
-    // symbols   = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+    static lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    static upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static numbers   = "0123456789";
+    static symbols   = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
 
-    // length    = document.getElementById("passwordLength").value;
+    length = document.getElementById("passwordLength").value;
 
-    get UpperCase(){
-            return this.upperCase[Math.floor(Math.random() * this.upperCase.length)]
+    // Speichert alle Getter-Funktionen in einem Array, damit später per random 
+    // Funktion ein getter ausgewählt werden kann.
+    getKey = [this.upperCase, this.lowerCase, this.num, this.symbol]
 
-
+    get upperCase(){
+        return Password.upperCase[Math.floor(Math.random() * Password.upperCase.length)]
         }
-        
+    get lowerCase(){
+        return Password.lowerCase[Math.floor(Math.random() * Password.lowerCase.length)]
+    }
+    get num(){
+        return Password.numbers[Math.floor(Math.random() * Password.numbers.length)]
+    }
+    get symbol(){
+        return Password.symbols[Math.floor(Math.random() * Password.symbols.length)]
+    }
+    password = ""
+    // createPassword (){
+    //     for (var i=0; i<this.length; i++){
+    //         key = this.getKey[Math.floor(Math.random() * this.getKey.length)]
+    //         console.log(key)
+    //     }
+}
     
 
-
+function createPassword(){
+    const newPass = new Password;
+        
+    for (var i=0; i<newPass.length; i++){
+        key = newPass.getKey[Math.floor(Math.random() * newPass.getKey.length)]
+        newPass.password += key
+        console.log("Random Key Durchlauf " + i + ": " + key)
+    }
+    console.log(newPass.password)
 }
+createPassword()
 
+const newPass2 = new Password;
+// console.log(newPass2);
 
-const newPass = new Password;
-console.log(newPass);
-
-let x = newPass.UpperCase;
-console.log(x);
+//Teststation
+let x = newPass2.upperCase
+console.log(x)
+let y = newPass2.lowerCase
+console.log(y)
+let z = newPass2.symbol
+console.log(z)
+let a = newPass2.num
+console.log(a)
+// let randomGetter = newPass2.getKey[Math.floor(Math.random() * newPass2.getKey.length)]
+// console.log(randomGetter)
 
